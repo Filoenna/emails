4dc573c527db8db73548fe94691f930f1398ae56
Zuzanna <machefi1@gmail.com>
2022-04-18 10:18:12 +0200

    Add sendind emails from different templates

--- app/send_email.py
+++ app/send_email.py
@@ -1,54 +1,136 @@
-import os
-from fastapi import BackgroundTasks
+from re import template
+from fastapi import APIRouter, Request, HTTPException, Depends, BackgroundTasks
+from fastapi_mail import ConnectionConfig
+import requests
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import EmailStr, BaseModel
+from typing import List, Dict, Any
+from pathlib import Path
+from fastapi.responses import JSONResponse
+import os
+
 from dotenv import load_dotenv
 
-load_dotenv(".env")
+load_dotenv()
 
+router = APIRouter()
 
-class Envs:
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-    MAIL_FROM = os.getenv("MAIL_FROM")
-    MAIL_PORT = int(os.getenv("MAIL_PORT"))
-    MAIL_SERVER = os.getenv("MAIL_SERVER")
-    MAIL_FROM_NAME = os.getenv("MAIN_FROM_NAME")
-
-
-conf = ConnectionConfig(
-    MAIL_USERNAME=Envs.MAIL_USERNAME,
-    MAIL_PASSWORD=Envs.MAIL_PASSWORD,
-    MAIL_FROM=Envs.MAIL_FROM,
-    MAIL_PORT=Envs.MAIL_PORT,
-    MAIL_SERVER=Envs.MAIL_SERVER,
-    MAIL_FROM_NAME=Envs.MAIL_FROM_NAME,
-    MAIL_TLS=True,
-    MAIL_SSL=False,
-    USE_CREDENTIALS=True,
-    TEMPLATE_FOLDER="./templates/email",
+router = APIRouter(
+    prefix="/email",
+    tags=["emails"],
+    responses={404: {"description": "Not found"}},
 )
 
 
-async def send_email_async(subject: str, email_to: str, body: dict):
-    message = MessageSchema(
-        subject=subject,
-        recipients=[email_to],
-        body=body,
-        subtype="html",
-    )
-
-    fm = FastMail(conf)
-    await fm.send_message(message, template_name="email.html")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-def send_email_background(
-    background_tasks: BackgroundTasks, subject: str, email_to: str, body: dict
+conf = ConnectionConfig(
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM=os.getenv("MAIL_FROM"),
+    MAIL_PORT=int(os.getenv("MAIL_PORT")),
+    MAIL_SERVER=os.getenv("MAIL_SERVER"),
+    MAIL_FROM_NAME=os.getenv("MAIN_FROM_NAME"),
+    MAIL_TLS=True,
+    MAIL_SSL=False,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
+    TEMPLATE_FOLDER=Path(__file__).parent / "templates",
+)
+
+html = """
+    <h4 style="color: magenta;">Hello Stranger!</h4>
+    <p>This email is just for test.</p>
+    <p><em>Filoenna</em></p>
+"""
+
+
+class EmailSchema(BaseModel):
+    email: List[EmailStr]
+    body: str
+    subject: str
+
+
+class EmailSchemaForTemplates(BaseModel):
+    email: List[EmailStr]
+    body: Dict[str, Any]
+    subject: str
+    template: str
+
+
+def sso(
+    APIKey: str = Depends(oauth2_scheme),
 ):
+    try:
+        validation = requests.post(
+            "http://127.0.0.1:5000/dj-rest-auth/token/verify/",
+            data={"token": APIKey},
+        )
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=e)
+    else:
+        if validation.status_code != 200:
+            raise HTTPException(status_code=401, detail=validation.text)
+    return True
+
+
+@router.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    print(form_data.username)
+    print(form_data.password)
+    data = {"email": form_data.username, "password": form_data.password}
+    sso = requests.post(
+        "http://127.0.0.1:5000/dj-rest-auth/login/",
+        data,
+    )
+    content = sso.json()
+    print(content["access_token"])
+    if sso.status_code != 200:
+        raise HTTPException(
+            status_code=401,
+        )
+    return content["access_token"]
+
+
+@router.post("/send")
+async def email(
+    request: Request, background_tasks: BackgroundTasks, email: EmailSchema
+) -> JSONResponse:
+    data = await request.json()
+    token = data.get("token")
+    sso(token)
+    print(email.dict)
     message = MessageSchema(
-        subject=subject,
-        recipients=[email_to],
-        body=body,
+        subject=email.dict().get("subject"),
+        recipients=email.dict().get("email"),
+        body=email.dict().get("body"),
+    )
+
+    fm = FastMail(conf)
+
+    background_tasks.add_task(fm.send_message, message)
+
+    return JSONResponse(status_code=200, content={"message": "email has been sent"})
+
+
+@router.post("/sendtemplate")
+async def email_from_template(
+    request: Request, email: EmailSchemaForTemplates
+) -> JSONResponse:
+    data = await request.json()
+    token = data.get("token")
+    sso(token)
+    message = MessageSchema(
+        subject=email.dict().get("subject"),
+        recipients=email.dict().get("email"),
+        template_body=email.dict().get("body"),
         subtype="html",
     )
+
     fm = FastMail(conf)
-    background_tasks.add_task(fm.send_message, message, template_name="email.html")
+
+    await fm.send_message(message, template_name=data.get("template") + ".html")
+
+    return JSONResponse(status_code=200, content={"message": "email has been sent"})
